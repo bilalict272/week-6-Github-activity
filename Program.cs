0887961d267b0b8760ae0d36e9627a56d2f94337
bilalict272 <bali7445@gmail.com>
2025-05-15 21:52:39 +0500

    Added Prime and Composite number checks

--- Program.cs
+++ Program.cs
@@ -4,6 +4,20 @@ namespace Week6GitHubActivity
 {
     class Program
     {
+               static bool IsPrime(int num)
+              {
+             if (num <= 1) return false;
+             for (int i = 2; i <= Math.Sqrt(num); i++)
+             {
+                 if (num % i == 0) return false;
+              }
+              return true;
+             }
+
+            static bool IsComposite(int num)
+            {
+               return num > 1 && !IsPrime(num);
+             }
         static void Main(string[] args)
         {
             Console.Write("Enter a number: ");
@@ -17,6 +31,21 @@ namespace Week6GitHubActivity
             {
                 Console.WriteLine(number + " is Odd.");
             }
+            if (IsPrime(number))
+          {
+                 Console.WriteLine(number + " is a Prime number.");
+          }
+         else if (IsComposite(number))
+           {
+                 Console.WriteLine(number + " is a Composite number.");
+          }
+           else
+          {
+                 Console.WriteLine(number + " is neither Prime nor Composite.");
+            }
+
+     
+
         }
     }
 }
